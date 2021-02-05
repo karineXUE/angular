e3df6ae6e033f6fd59a6a96729929fff63a46ed9
Karine Xue <karine.xue@grenet.fr>
2021-02-05 17:27:10 +0100

    create project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MenuAideComponent } from './component/menu-aide/menu-aide.component';
+import { PageNotFoundComponent } from './component/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/menu', pathMatch: 'full' },
+  { path: 'menu', component: MenuAideComponent },
+
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+
+  imports: [
+    RouterModule.forRoot(
+      routes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
+  ],
+
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MatMenuModule} from '@angular/material/menu'; 
+
+import { MessagesComponent } from './component/messages/messages.component';
+import { MenuAideComponent } from './component/menu-aide/menu-aide.component';
+import { PageNotFoundComponent } from './component/page-not-found/page-not-found.component';
+import { HttpClientModule } from '@angular/common/http';
+import { SousTypeAideComponent } from './component/sous-type-aide/sous-type-aide.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MessagesComponent,
+    MenuAideComponent,
+    PageNotFoundComponent,
+    SousTypeAideComponent
   ],
+
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    NoopAnimationsModule,
+    MatMenuModule
   ],
-  providers: [],
+  //providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
